850c5064b70478639280bda5824b72d62d0d99f0
JuandAvendanog <141972525+JuandAvendanog@users.noreply.github.com>
2024-03-23 09:57:16 -0500

    quickSort

--- quick_sort.py
+++ quick_sort.py
@@ -1,3 +1,10 @@
 class QuickSort():
-    def quickSort(input) -> list:
-        return input
+    
+    def quickSort(input) -> list:  
+        if len(input) < 2:  
+            return input  
+        pivot = input[0]  
+        left = [x for x in input[1:] if x < pivot]  
+        right = [x for x in input[1:] if x >= pivot]  
+        return QuickSort.quickSort(left) + [pivot] + QuickSort.quickSort(right)  
+
