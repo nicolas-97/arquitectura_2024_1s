f6ce0c70a3dd95480b8c13392e32b603424e14f4
preuts1308 <141975215+preuts1308@users.noreply.github.com>
2024-03-23 09:49:31 -0500

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,3 +1,12 @@
-class QuickSort():
-    def quickSort(input) -> list:
-        return input
+class QuickSort:
+    @staticmethod
+    def quickSort(input_list):
+        if len(input_list) <= 1:
+            return input_list
+
+        pivot = input_list[-1]
+
+        smaller = [x for x in input_list if x < pivot]
+        larger = [x for x in input_list if x > pivot]
+
+        return QuickSort.quickSort(smaller) + [pivot] + QuickSort.quickSort(larger)
