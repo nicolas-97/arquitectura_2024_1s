328bab84dccc7bf77050b33247efc552742506c3
andressandoval08 <andresf.sandovalp@utadeo.edu.co>
2024-03-23 09:49:39 -0500

    Se agrega al repositorio

--- quick_sort.py
+++ quick_sort.py
@@ -1,3 +1,26 @@
-class QuickSort():
+class QuickSort:
+    @staticmethod
+    def particionar(arr, bajo, alto):
+        pivote = arr[alto]
+        i = bajo - 1
+        for j in range(bajo, alto):
+            if arr[j] < pivote:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i + 1], arr[alto] = arr[alto], arr[i + 1]
+        return i + 1
+
+    @staticmethod
+    def ordenar_rapido(arr, bajo, alto):
+        if bajo < alto:
+            pi = QuickSort.particionar(arr, bajo, alto)
+            QuickSort.ordenar_rapido(arr, bajo, pi - 1)
+            QuickSort.ordenar_rapido(arr, pi + 1, alto)
+
+    @staticmethod
     def quickSort(input) -> list:
-        return input
+        arr = input.copy()
+        n = len(arr)
+        QuickSort.ordenar_rapido(arr, 0, n - 1)
+        return arr
+
